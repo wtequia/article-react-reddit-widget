5cd5f88ac80b6d0872e9629b7c984250ba38e32f
Wilmer Ferney Tequia Rojas <wtequia@elespectador.com>
2020-10-13 19:16:39 -0500

    update links

---
+++ _next/static/webpack/3.5c3771de2829751455e7.hot-update.js
@@ -0,0 +1,97 @@
+webpackHotUpdate_N_E(3,{
+
+/***/ "./node_modules/@babel/runtime/helpers/arrayWithHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/asyncToGenerator.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/construct.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/interopRequireWildcard.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/isNativeReflectConstruct.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/iterableToArrayLimit.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/nonIterableRest.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/slicedToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/regenerator/index.js":
+false,
+
+/***/ "./node_modules/next/dist/client/link.js":
+false,
+
+/***/ "./node_modules/next/dist/client/normalize-trailing-slash.js":
+false,
+
+/***/ "./node_modules/next/dist/client/router.js":
+false,
+
+/***/ "./node_modules/next/dist/client/with-router.js":
+false,
+
+/***/ "./node_modules/next/dist/compiled/path-to-regexp/index.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/mitt.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/router.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/format-url.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/is-dynamic.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/parse-relative-url.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/path-match.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/prepare-destination.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/querystring.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/resolve-rewrites.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-matcher.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-regex.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/utils.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/server/denormalize-page-path.js":
+false,
+
+/***/ "./node_modules/next/link.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime.js":
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay8zLjVjMzc3MWRlMjgyOTc1MTQ1NWU3LmhvdC11cGRhdGUuanMiLCJzb3VyY2VSb290IjoiIn0=
