531f2cf83149b7623706f31660527aa9a426f942
wtequia <62073205+wtequia@users.noreply.github.com>
2021-12-29 15:23:34 -0500

    Update test.js

--- assets/test.js
+++ assets/test.js
@@ -1,7 +1,112 @@
 console.log("esto es una prueba");
 
 document.getElementById('headerDiv').innerHTML =`
-                    <div id="__next">
+                 <meta name="viewport" content="width=device-width" />
+    <meta charset="utf-8" />
+    <link rel="preconnect" href="https://use.typekit.net/fri7zxf.css" />
+    <link as="style" href="https://use.typekit.net/fri7zxf.css" rel="preload" />
+    <link href="https://use.typekit.net/fri7zxf.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://use.typekit.net/fri7zxf.css" />
+    <link as="style" href="https://use.typekit.net/fri7zxf.css" rel="preload" />
+    <link href="https://use.typekit.net/fri7zxf.css" rel="stylesheet" />
+    <link
+      rel="preload"
+      href="//www.googletagservices.com/tag/js/gpt.js"
+      as="script"
+    />
+    <link
+      rel="preconnect"
+      href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,900&amp;display=swap"
+      crossorigin="true"
+    />
+    <link
+      as="style"
+      href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,900&amp;display=swap"
+      crossorigin="true"
+      rel="preload"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,900&amp;display=swap"
+      crossorigin="true"
+      rel="stylesheet"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/css/3b9f23e1af39fbc4acfe.css"
+      as="style"
+    />
+    <link
+      rel="stylesheet"
+      href="/_next/static/css/3b9f23e1af39fbc4acfe.css"
+      data-n-g=""
+    />
+    <link
+      rel="preload"
+      href="/_next/static/css/0386f00a4889cda1ae90.css"
+      as="style"
+    />
+    <link
+      rel="stylesheet"
+      href="/_next/static/css/0386f00a4889cda1ae90.css"
+      data-n-g=""
+    />
+    <link
+      rel="preload"
+      href="/_next/static/css/afd7172b7cfc566ac23d.css"
+      as="style"
+    />
+    <link
+      rel="stylesheet"
+      href="/_next/static/css/afd7172b7cfc566ac23d.css"
+      data-n-p=""
+    />
+    <noscript data-n-css="true"></noscript>
+    <link
+      rel="preload"
+      href="/_next/static/chunks/main-c96340488a2a83a62d07.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/webpack-e067438c4cf4ef2ef178.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/framework.964e76377795026233d5.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/3e86aeee.ff5578978733a40a67a3.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/f6078781a05fe1bcb0902d23dbbb2662c8d200b3.34a880cd93b71b6c37f4.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/pages/_app-3f8f5c4f084b3a108433.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/d1ac1aa7.8dd2bcc10e300262580a.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/8bf380cd5dec50c9cd192153689475ed8b036d6c.b67d4c2f8ac8c14da9e5.js"
+      as="script"
+    />
+    <link
+      rel="preload"
+      href="/_next/static/chunks/pages/index-702daf3909c845367da0.js"
+      as="script"
+    />
+    <div id="__next">
       <div>
         <script
           defer=""
@@ -287,6 +392,152 @@ document.getElementById('headerDiv').innerHTML =`
         </div>
       </div>
     </div>
+    <script id="__NEXT_DATA__" type="application/json">
+      {
+        "props": { "pageProps": {} },
+        "page": "/",
+        "query": {},
+        "buildId": "yZGT6WCLGyrEGVnyx1e4j",
+        "nextExport": true,
+        "autoExport": true,
+        "isFallback": false,
+        "head": [
+          ["meta", { "name": "viewport", "content": "width=device-width" }],
+          ["meta", { "charSet": "utf-8" }],
+          [
+            "link",
+            {
+              "rel": "preconnect",
+              "href": "https://use.typekit.net/fri7zxf.css",
+              "crossOrigin": true
+            }
+          ],
+          [
+            "link",
+            {
+              "as": "style",
+              "href": "https://use.typekit.net/fri7zxf.css",
+              "crossOrigin": true,
+              "rel": "preload"
+            }
+          ],
+          [
+            "link",
+            {
+              "href": "https://use.typekit.net/fri7zxf.css",
+              "crossOrigin": true,
+              "rel": "stylesheet"
+            }
+          ],
+          [
+            "link",
+            {
+              "rel": "preconnect",
+              "href": "https://use.typekit.net/fri7zxf.css",
+              "crossOrigin": true
+            }
+          ],
+          [
+            "link",
+            {
+              "as": "style",
+              "href": "https://use.typekit.net/fri7zxf.css",
+              "crossOrigin": true,
+              "rel": "preload"
+            }
+          ],
+          [
+            "link",
+            {
+              "href": "https://use.typekit.net/fri7zxf.css",
+              "crossOrigin": true,
+              "rel": "stylesheet"
+            }
+          ],
+          [
+            "link",
+            {
+              "rel": "preload",
+              "href": "//www.googletagservices.com/tag/js/gpt.js",
+              "as": "script"
+            }
+          ],
+          [
+            "link",
+            {
+              "rel": "preconnect",
+              "href": "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,900\u0026display=swap",
+              "crossOrigin": "true"
+            }
+          ],
+          [
+            "link",
+            {
+              "as": "style",
+              "href": "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,900\u0026display=swap",
+              "crossOrigin": "true",
+              "rel": "preload"
+            }
+          ],
+          [
+            "link",
+            {
+              "href": "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,900\u0026display=swap",
+              "crossOrigin": "true",
+              "rel": "stylesheet"
+            }
+          ]
+        ]
+      }
+    </script>
+    <script
+      nomodule=""
+      src="/_next/static/chunks/polyfills-6e6c1838cce5cdc92ffa.js"
+    ></script>
+    <script
+      src="/_next/static/chunks/main-c96340488a2a83a62d07.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/webpack-e067438c4cf4ef2ef178.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/framework.964e76377795026233d5.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/3e86aeee.ff5578978733a40a67a3.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/f6078781a05fe1bcb0902d23dbbb2662c8d200b3.34a880cd93b71b6c37f4.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/pages/_app-3f8f5c4f084b3a108433.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/d1ac1aa7.8dd2bcc10e300262580a.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/8bf380cd5dec50c9cd192153689475ed8b036d6c.b67d4c2f8ac8c14da9e5.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/chunks/pages/index-702daf3909c845367da0.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/yZGT6WCLGyrEGVnyx1e4j/_buildManifest.js"
+      async=""
+    ></script>
+    <script
+      src="/_next/static/yZGT6WCLGyrEGVnyx1e4j/_ssgManifest.js"
+      async=""
+    ></script>
   `;
 
 document.getElementById('prueba2').innerHTML = `  <div>
