9e3358b8bfc4e3a8bcf873e120f23235494730d9
wtequia <62073205+wtequia@users.noreply.github.com>
2021-12-29 10:25:08 -0500

    Update test.js

--- assets/test.js
+++ assets/test.js
@@ -1,1 +1,3 @@
 console.log("esto es una prueba");
+document.getElementById('prueba1').innerHTML += '<div id="idChild"> prueba 1 </div>';
+document.getElementById('prueba2').innerHTML += '<div id="idChild"> prueba 2 </div>';
